772b5c2c23a338aac1ccdf3ec1621a27751496bf
Zerschta <Sauerjunior@gmx.net>
2026-09-10 12:20:03 +0200

    assets

--- Assets/Scripts/Player/bulletspawner.cs
+++ Assets/Scripts/Player/bulletspawner.cs
@@ -3,14 +3,6 @@ using UnityEngine;
 public class bulletspawner : MonoBehaviour
 {    
     public GameObject bullet;
-
-    bool PressedSpace;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
